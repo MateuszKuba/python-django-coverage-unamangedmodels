a55e5113e25a14c67a219f15143512ea27ecd285
Mateusz Kuba <mateusz.kuba@onet.eu>
2021-05-19 11:52:07 +0200

    add unmanaged model option

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,13 +28,13 @@ fi
 
 source "${GITHUB_WORKSPACE}/${VENV_NAME}/bin/activate"
 
-pip install -r requirements.txt
+pip install -r requirements/local.txt
 
 echo "Base setup complete. Setting up a sample DB url and running..."
 export DATABASE_URL='postgresql://ctest:coveragetest123@127.0.0.1:5432/demo'
 
 # This will automatically fail (set -e is set by default) if the tests fail, which is OK.
-coverage run --source "${APP_LOCATION}" manage.py test "${APP}"
+coverage run --source "${APP_LOCATION}" manage.py test -n "${APP}"
 
 # Now get the coverage
 COVERAGE_RESULT=`coverage report | grep TOTAL | awk 'N=1 {print $NF}' | sed 's/%//g'`
